1a00bcb1e70770dfa648f9f615ac82f7ad36d171
Fredcraft <146226959+Fredcraft19@users.noreply.github.com>
2026-07-14 20:35:49 +0100

    Made Mini-Object
    This is for basic memory so when a statement like this is make:
    if (1 > 0)
    The interpreter sees the ) and stores the token (Token.IF) and t he condition (1 > 0) as a mini-object (meaning mini-nested-object).
    So then when the { is spotted, the if statement and check if it should run the next code or not from the mini-object being turned into a NestedObject

--- CSoft/interpreter/memory.js
+++ CSoft/interpreter/memory.js
@@ -29,11 +29,29 @@ class NestingObject{
     }
 }
 
+class MiniObject{
+    token = null;
+    args = [];
+    constructor(token, args){
+        this.token = token;
+        this.args = args;
+    }
+    Token(){
+        return this.token;
+    }
+    Args(){
+        return this.args;
+    }
+    FirstArgs(){
+        return this.args[0];
+    }
+}
+
 class Memory{
     // Map of all variables, KEY = "VAR_NAME" VALUE = "value"-if primitive / "class instance"-if custom
     // global variables always in scope
     static variables = {
-        csoft_version:"0.0.1"
+        csoft_version:new Variable("0.0.1")
     };
 
     static depth = 0;
@@ -42,22 +60,7 @@ class Memory{
 
     static Reset(){
         this.variables = {
-            csoft_version:"0.0.1"
+            "csoft_version":new Variable("0.0.1")
         };
-    }
-
-    static AddNesting(){
-
-    }
-    static OutNesting(){
-        if(this.nest_value > 0){
-            this.nest_value--;
-        }
-        to_delete = [];
-        for(let i = 0; i < this.variables.length; i++){
-            if(this.variables[i].nest > this.nest_value){
-                to_delete.push(this.variables[i]);
-            }
-        }
     }
 }
